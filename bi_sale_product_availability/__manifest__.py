df1aa06184a3cf57b5ddceb43f2010b7b0025920
FatmaDaify <fdaify@iskydev.com>
2019-04-25 20:17:10 +0200

    [ADD] sale product availablity module that add constrain in sale order line if ordered qty greater than avaliable in location

---
+++ bi_sale_product_availability/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "BI Sale Product Availability",
+    'summary': "BI Sale Product Availability",
+    'description': """ 
+            This module adds constrain on product in sale order line if requested qty less than available qty in location.
+     """,
+    'author': "BI Solutions Development Team",
+    'category': 'Sale',
+    'version': '0.1',
+    'depends': ['sale_management', 'stock'],
+    'data': [
+        'views/sale_order_inherit_view.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'sequence': 1
+}
