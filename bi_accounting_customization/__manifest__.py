ce9c3da7d088fe980a3906fbf229819e11927196
khairy <mkhairy@iskydev.com>
2019-04-26 13:27:33 +0200

    [ADD] accounting customization

---
+++ bi_accounting_customization/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Bi Accounting Customizations",
+
+    'summary': """Bi Invoice Customizations""",
+
+    'description': """
+        1- Hide the default discount column from the invoice layout and adding a new discounts column based on the price list.\n
+        2- \n
+
+    """,
+
+    'author': "Bi Development Team",
+    'website': "http://bisolutions.com/",
+    'category': 'Account',
+    'version': '12.0.0.1.0',
+    'depends': ['account', 'account_accountant', 'stock'],
+
+    # always loaded
+    'data': [
+        # 'reports/invoices.xml',
+    ],
+}
