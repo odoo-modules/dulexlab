d802c8217f43378e8a711fa08171f29a4a0c68f4
khairy <mkhairy@iskydev.com>
2019-04-30 13:55:47 +0200

    [ADD] partners, memos to tree view batch.payemnt

--- bi_check_customization/models/account_batch_inherit.py
+++ bi_check_customization/models/account_batch_inherit.py
@@ -9,6 +9,23 @@ class BiAccountBatchPaymentInherit(models.Model):
         [('draft', 'New'), ('sent', 'Sent'), ('under_collection', 'Under Collection'), ('collection', 'Collection'),
          ('reconciled', 'Reconciled')], readonly=True, default='draft', copy=False)
 
+    partners_ids = fields.Many2many('res.partner', relation='partners_account_batch_rel', column1='batch_id',
+                                    column2='partner_id', string="Partners", compute='get_lines_data', store=True)
+
+    memos_lines = fields.Text('Memos', compute='get_lines_data', store=True)
+
+    @api.depends('payment_ids')
+    def get_lines_data(self):
+        for record in self:
+            record.partners_ids = record.payment_ids.mapped('partner_id.id')
+
+            memos = []
+            s = ', '
+            for line in record.payment_ids:
+                if line.communication:
+                    memos.append(line.communication)
+            record.memos_lines = s.join(memos)
+
     @api.multi
     def action_account_entries(self):
         return {
