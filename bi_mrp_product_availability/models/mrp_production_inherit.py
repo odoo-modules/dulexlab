f022273833a9e4550c0abbe9d23cabb7e1cfc4d5
FatmaSharkawy <fdaify@iskyev.com>
2019-05-08 22:19:21 +0200

    [UPD] mrp constrain

--- bi_mrp_product_availability/models/mrp_production_inherit.py
+++ bi_mrp_product_availability/models/mrp_production_inherit.py
@@ -11,7 +11,7 @@ class MrpProduction(models.Model):
         self.ensure_one()
         for stock_move in self.move_raw_ids:
             location_qty_available = stock_move.product_id.with_context(
-                location=stock_move.raw_material_production_id.location_src_id.id).virtual_available
+                location=stock_move.raw_material_production_id.location_src_id.id).qty_available
             if stock_move.product_uom_qty > location_qty_available:
                 raise ValidationError(
                     _('You plan to produce %s %s of %s but you only have %s %s available in %s location.') % \
@@ -25,7 +25,7 @@ class MrpProduction(models.Model):
         for production in self:
             for stock_move in production.move_raw_ids:
                 location_qty_available = stock_move.product_id.with_context(
-                    location=stock_move.raw_material_production_id.location_src_id.id).virtual_available
+                    location=stock_move.raw_material_production_id.location_src_id.id).qty_available
                 if stock_move.product_uom_qty > location_qty_available:
                     raise ValidationError(
                         _('You plan to produce %s %s of %s but you only have %s %s available in %s location.') % \
