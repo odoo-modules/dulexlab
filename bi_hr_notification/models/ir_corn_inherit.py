a9ef11a827aee351642c8ac07b071cfb1a24dac3
khairy <mkhairy@iskydev.com>
2019-05-22 15:22:15 +0200

    [UPD] notofication

--- bi_hr_notification/models/ir_corn_inherit.py
+++ bi_hr_notification/models/ir_corn_inherit.py
@@ -16,47 +16,57 @@ class IrCrone(models.Model):
     def employee_expiration_mail(self):
         get_param = self.env['ir.config_parameter'].sudo().get_param
         check_id_expiration = int(get_param('check_id_expiration'))
-        if check_id_expiration:
-            employee_objs = self.env['hr.employee'].sudo().search([])
-            for manager in employee_objs.mapped('parent_id'):
-                body = ''
-                current_date = fields.date.today()
-                table_header = "<table class='table'><thead><tr><th scope='col'>Employee Name</th><th scope='col'>Job Position</th><th scope='col'>Expiration Date</th><th scope='col'>Diff. Days</th></tr></thead><tbody>"
-                for employee in self.env['hr.employee'].sudo().search(
-                        [('parent_id', '=', manager.id), ('id_expiry_date', '!=', False)]):
-                    id_expiry_date = employee.id_expiry_date
-                    diff_date = (id_expiry_date - current_date).days
-                    if diff_date > 0 < check_id_expiration:
-                        emp_link = str("<a href=#id=" + str(
-                            employee.id) + "&view_type=form&model=hr.employee>" + employee.name + "</a>")
-                        body += str(
-                            "<tr><th scope='row'>" + emp_link + "</th><td>" + str(
-                                employee.job_id.name or ' ') + "</td><td>" + str(
-                                employee.id_expiry_date) + "</td><td style='color:red'>" + str(
-                                diff_date) + "</td></tr>")
+        user_lst = []
+        partner_ids = []
+        for user in self.env['res.users'].search([]):
+            if (user.has_group('hr.group_hr_manager')) and (user not in user_lst):
+                user_lst.append(user)
+                partner_ids.append(user.partner_id.id)
 
-                etable = "</tbody></table>"
-                vals = {'subject': 'Identification Expiry Date Mail **',
-                        'body_html': 'Dear ' + str(manager.name) + ',<br/>' + str(table_header) + str(body) + str(
-                            etable),
-                        'email_to': manager.work_email,
-                        'author_id': self.env.ref('base.partner_admin').id,
-                        }
+        if check_id_expiration and partner_ids:
+            body = ''
+            current_date = fields.date.today()
+            table_header = "<table class='table'><thead><tr><th scope='col'>Employee Name</th><th scope='col'>Job Position</th><th scope='col'>Expiration Date</th><th scope='col'>Diff. Days</th></tr></thead><tbody>"
+            for employee in self.env['hr.employee'].sudo().search([('id_expiry_date', '!=', False)]):
+                id_expiry_date = employee.id_expiry_date
+                diff_date = (id_expiry_date - current_date).days
+                if diff_date > 0 < check_id_expiration:
+                    emp_link = str("<a href=#id=" + str(
+                        employee.id) + "&view_type=form&model=hr.employee>" + employee.name + "</a>")
+                    body += str(
+                        "<tr><th scope='row'>" + emp_link + "</th><td>" + str(
+                            employee.job_id.name or ' ') + "</td><td>" + str(
+                            employee.id_expiry_date) + "</td><td style='color:red'>" + str(
+                            diff_date) + "</td></tr>")
 
-                mail = self.env['mail.mail'].create(vals)
-                mail.send()
+            etable = "</tbody></table>"
+            vals = {'subject': 'Identification Expiry Date Mail **',
+                    'body_html': 'Dears' + ',<br/>' + str(table_header) + str(body) + str(
+                        etable),
+                    'recipient_ids': [(6, 0, partner_ids)],
+                    'author_id': self.env.ref('base.partner_admin').id,
+                    }
+            mail = self.env['mail.mail'].create(vals)
+            mail.send()
 
     @api.multi
     def contract_send_email(self):
         get_param = self.env['ir.config_parameter'].sudo().get_param
         check_contract_expiration = int(get_param('check_contract_expiration'))
-        if check_contract_expiration:
-            contract_objs = self.env['hr.contract'].sudo().search(
-                [('state', '=', 'open'), ('date_end', '!=', False), ('employee_id.parent_id', '!=', False)])
+        user_lst = []
+        partner_ids = []
+        for user in self.env['res.users'].search([]):
+            if (user.has_group('hr.group_hr_manager')) and (user not in user_lst):
+                user_lst.append(user)
+                partner_ids.append(user.partner_id.id)
+
+        if check_contract_expiration and partner_ids:
+            contract_objs = self.env['hr.contract'].sudo().search([('state', '=', 'open'), ('date_end', '!=', False)])
+            body = ''
+            table_header = "<table class='table'><thead><tr><th scope='col'>Employee Name</th><th scope='col'>Contract Date</th><th scope='col'>Expiration Date</th><th scope='col'>Diff. Days</th></tr></thead><tbody>"
+
             for contract in contract_objs:
-                body = ''
                 current_date = fields.date.today()
-                table_header = "<table class='table'><thead><tr><th scope='col'>Employee Name</th><th scope='col'>Contract Date</th><th scope='col'>Expiration Date</th><th scope='col'>Diff. Days</th></tr></thead><tbody>"
 
                 contract_expiry_date = contract.date_end
                 diff_date = (contract_expiry_date - current_date).days
@@ -68,13 +78,13 @@ class IrCrone(models.Model):
                             contract.date_start or ' ') + "</td><td>" + str(
                             contract.date_end or ' ') + "</td><td style='color:red'>" + str(diff_date) + "</td></tr>")
 
-                etable = "</tbody></table>"
-                vals = {'subject': 'Contract Expiry Date Mail **',
-                        'body_html': 'Dear ' + str(contract.employee_id.parent_id.name) + ',<br/>' + str(
-                            table_header) + str(body) + str(etable),
-                        'email_to': contract.employee_id.parent_id.id,
-                        'author_id': self.env.ref('base.partner_admin').id,
-                        }
+            etable = "</tbody></table>"
+            vals = {'subject': 'Contract Expiry Date Mail **',
+                    'body_html': 'Dear ' + str(contract.employee_id.parent_id.name) + ',<br/>' + str(
+                        table_header) + str(body) + str(etable),
+                    'recipient_ids': [(6, 0, partner_ids)],
+                    'author_id': self.env.ref('base.partner_admin').id,
+                    }
 
-                mail = self.env['mail.mail'].create(vals)
-                mail.send()
+            mail = self.env['mail.mail'].create(vals)
+            mail.send()
