93d73055e895940e5e3ddc7b9092a182842a7577
FatmaSharkawy <fdaify@iskyev.com>
2019-06-17 00:32:57 +0200

    [UPD] customer credit limit to make it checbox not selection depend on amount or open invoices

--- bi_customer_credit_limit/models/res_partner_inherit.py
+++ bi_customer_credit_limit/models/res_partner_inherit.py
@@ -7,15 +7,34 @@ class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     use_credit_limit = fields.Boolean(string='Credit Limit')
-    credit_limit_type = fields.Selection([('amount', 'Amount'), ('open_invoices', 'Open Invoices')],
-                                         string='Credit Limit Type')
-    allowed_amount = fields.Float(string='Credit Limit Amount')
-    allowed_invoice_numbers = fields.Integer(string='Invoices Number')
+    credit_limit_amount = fields.Boolean('Credit Limit On Amount')
+    credit_limit_open_invoices = fields.Boolean('Credit Limit On Open Invoices')
+    allowed_amount = fields.Float(string='Allowed Amount')
+    allowed_invoice_numbers = fields.Integer(string='Allowed Invoices Number')
 
-    @api.onchange('credit_limit_type')
+    @api.multi
+    @api.constrains('allowed_amount')
+    def check_credit_limit_amount(self):
+        for rec in self:
+            if rec.credit_limit_amount and rec.allowed_amount <= 0.0:
+                raise ValidationError(_('Allowed amount must be greater than zero.'))
+
+    @api.multi
+    @api.constrains('allowed_invoice_numbers')
+    def check_credit_limit_invoice_numbers(self):
+        for rec in self:
+            if rec.credit_limit_open_invoices and rec.allowed_invoice_numbers <= 0:
+                print("bbbbbbbbbbbbbbbbb")
+                raise ValidationError(_('Allowed invoice number must be greater than zero.'))
+
+    @api.onchange('credit_limit_amount')
     def clear_allowed_amount(self):
         for rec in self:
-            if rec.credit_limit_type != 'amount':
+            if not rec.credit_limit_amount:
                 rec.allowed_amount = 0
-            elif rec.credit_limit_type != 'open_invoices':
+
+    @api.onchange('credit_limit_open_invoices')
+    def clear_allowed_invoice_number(self):
+        for rec in self:
+            if not rec.credit_limit_open_invoices:
                 rec.allowed_invoice_numbers = 0
