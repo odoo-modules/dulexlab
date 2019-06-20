d1594351f959b4c386b83928793fef4c35ea7187
ibrahim qabeel <ibrahimqabeell@gmail.com>
2019-06-20 02:14:58 +0200

    [UPD] Module: bi_accounting_rules update

--- bi_accounting_rules/models/account_payment.py
+++ bi_accounting_rules/models/account_payment.py
@@ -6,18 +6,3 @@ class AccountPayment(models.Model):
     _inherit = 'account.payment'
 
     responsible_user = fields.Many2one('res.users', string="Responsible User", related="journal_id.responsible_user")
-    to_view = fields.Boolean(string='To View Row', compute='to_view_value', store=False)
-    to_view_s = fields.Boolean(string='To View')
-
-    @api.multi
-    def to_view_value(self):
-        for record in self:
-            if self.user_has_groups('bi_accounting_rules.account_treasury_group_manager') \
-               or self.env.user.id == 2                              \
-               or record.responsible_user.id == self.env.user.id:
-                record.to_view = True
-                record.to_view_s = True
-            else:
-                record.to_view = False
-                record.to_view_s = False
-            print(record.to_view_s)
