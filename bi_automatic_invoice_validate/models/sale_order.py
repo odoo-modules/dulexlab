6432bb8d7d6c2e78b6f771f1c034bcd45ed87a37
ibrahim qabeel <ibrahimqabeell@gmail.com>
2019-06-23 11:32:53 +0200

    [ADD] Module bi_automatic_invoice_validate, [UPD] bi_sales_team_hierarchy

---
+++ bi_automatic_invoice_validate/models/sale_order.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from odoo import models, api, _
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    def action_invoice_create(self, grouped=False, final=False):
+        invoices = super(SaleOrder, self).action_invoice_create(grouped, final)
+        for invoice_id in invoices:
+            invoice = self.env['account.invoice'].browse(invoice_id)
+            invoice.action_invoice_open()
