f98be702291bf17d63e7d6cc6df65dc7a8b6f7c9
ibrahim qabeel <ibrahimqabeell@gmail.com>
2019-06-24 22:42:41 +0200

    [UPD] showing customer type in invoices report & removing default journal in register payment.

--- bi_customer_type/models/inherit_account_invoice.py
+++ bi_customer_type/models/inherit_account_invoice.py
@@ -6,3 +6,18 @@ class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
 
     customer_type = fields.Many2one('customer.type', string="Customer Type", related='partner_id.customer_type', store=True)
+
+
+class AccountInvoiceReport(models.Model):
+    _inherit = 'account.invoice.report'
+
+    customer_type = fields.Many2one('customer.type', string="Customer Type", related='partner_id.customer_type', store=True)
+
+    def _select(self):
+        return super(AccountInvoiceReport, self)._select() + ", sub.customer_type as customer_type"
+
+    def _sub_select(self):
+        return super(AccountInvoiceReport, self)._sub_select() + ", ai.customer_type as customer_type"
+
+    def _group_by(self):
+        return super(AccountInvoiceReport, self)._group_by() + ", ai.customer_type"
