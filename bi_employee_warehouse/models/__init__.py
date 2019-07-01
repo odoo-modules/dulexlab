8bdee92d5ef7ff41136166ed907cb124b27b9a34
ibrahim qabeel <ibrahimqabeell@gmail.com>
2019-07-01 16:30:50 +0200

    [UPD] Module: bi_employee_warehouse update.

--- bi_employee_warehouse/models/__init__.py
+++ bi_employee_warehouse/models/__init__.py
@@ -2,3 +2,4 @@
 
 from . import hr_employee_inherit
 from . import res_users_inherit
+from . import stock_picking_inherit
--- bi_employee_warehouse/models/stock_picking_inherit.py
+++ bi_employee_warehouse/models/stock_picking_inherit.py
@@ -8,4 +8,8 @@ class StockPickingInherit(models.Model):
 
     @api.multi
     def action_done(self):
-        return super(StockPickingInherit,self.sudo()).action_done()
+        return super(StockPickingInherit, self.sudo()).action_done()
+
+    @api.multi
+    def action_confirm(self):
+        return super(StockPickingInherit, self.sudo()).action_confirm()
