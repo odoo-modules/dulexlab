cf98552944472e76e5a7e2983c4671f649ffc66f
ibrahim qabeel <ibrahimqabeell@gmail.com>
2019-07-03 16:12:38 +0200

    [UPD] bi_sales_team_hirearchy & [ADD] Module: bi_delivery_note_report.

---
+++ bi_delivery_note_report/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'license': 'LGPL-3',
+    "name": "BI DulexLab Delivery Note",
+    "version": "1",
+    'sequence': 1,
+    "author": "BI Solutions Development Team",
+    "category": "DulexLab Pickings",
+    "summary": "This Module Add Delivery Note Report to stock picking.",
+    "depends": ["stock"],
+    "data": [
+        'report/stock_picking_report.xml',
+    ],
+    "installable": True
+}
--- bi_sales_team_hierarchy/models/inherit_stock_picking.py
+++ bi_sales_team_hierarchy/models/inherit_stock_picking.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields, api
 
+
 class InheritedStockPicking(models.Model):
     _inherit = 'stock.picking'
 
@@ -8,6 +9,30 @@ class InheritedStockPicking(models.Model):
     sales_team = fields.Many2one('crm.team', string="Sales Team", related='sale_id.team_id')
     area = fields.Many2one('new.area', string="Area", related='sale_id.area')
     team_supervisor = fields.Many2one('res.users', srting="Term Supervisor", related='sale_id.team_supervisor')
-    team_leader = fields.Many2one('res.users', srting="Term Leader", related='sale_id.team_leader')
-    driver_name = fields.Many2one('res.partner', string="Driver Name", related='sale_id.driver_name')
-    car_number = fields.Many2one('fleet.vehicle', string="Car Number", related='sale_id.car_number')
+    team_leader = fields.Many2one('res.users', srting="Term Leader")
+    driver_name = fields.Many2one('res.partner', string="Driver Name")
+    car_number = fields.Many2one('fleet.vehicle', string="Car Number")
+    is_from_sale = fields.Boolean(string='From Sale Order', compute='is_from_so')
+
+    @api.model
+    def create(self, vals):
+        res = super(InheritedStockPicking, self).create(vals)
+        if 'sale_id' in vals:
+            res.write({'car_number': res.sale_id.car_number.id, 'driver_name': res.sale_id.driver_name.id})
+        print(res.name)
+        return res
+
+    @api.multi
+    def is_from_so(self):
+        for pick in self:
+            if pick.sale_id:
+                pick.is_from_sale = True
+                pick.write({'is_from_sale': 'True',
+                            'car_number': pick.sale_id.car_number.id, 'driver_name': pick.sale_id.driver_name.id})
+
+    @api.onchange('car_number')
+    @api.multi
+    def set_driver_name(self):
+        for pick in self:
+            if pick.car_number and pick.car_number.driver_id and pick.car_number.driver_id.id:
+                pick.driver_name = pick.car_number.driver_id.id
