c8529243083642a633dc1586bfa431ef95d424b8
Mustafa <mustafa.cu13@yahoo.com>
2019-07-06 13:34:52 +0200

    [UPD] employee/contract form fields and notifications

--- bi_hr_modification/models/hr_contract_inherit.py
+++ bi_hr_modification/models/hr_contract_inherit.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
+
 from odoo import models, fields, api, _
-from odoo.exceptions import ValidationError
-from datetime import date
+from dateutil.relativedelta import relativedelta
 
 
 class HrContractInherit(models.Model):
@@ -9,3 +9,12 @@ class HrContractInherit(models.Model):
 
     insurance = fields.Float('Insurance')
     tax = fields.Float('Tax')
+
+    @api.onchange('date_start')
+    def _onchange_date_start(self):
+        if self.date_start:
+            self.date_end =  self.date_start + relativedelta(years=1)
+            self.trial_date_end =  self.date_start + relativedelta(months=3)
+        else:
+            self.date_end = False
+            self.trial_date_end = False
