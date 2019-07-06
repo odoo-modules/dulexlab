c4fbf7d183356db7ffa0eea2e641b2af4770fb18
khairy <mkhairy@iskydev.com>
2019-07-06 16:39:06 +0200

    [UPD] InvReport

--- bi_invoice_sequence/models/inherit_account_invoice.py
+++ bi_invoice_sequence/models/inherit_account_invoice.py
@@ -6,17 +6,27 @@ import uuid
 from odoo.exceptions import AccessError, UserError, RedirectWarning, ValidationError, Warning
 
 from openerp.exceptions import ValidationError
+from num2words import num2words
 
 
 class AccountInvoiceInhh(models.Model):
     _inherit = 'account.invoice'
 
-    seq_un_taxed = fields.Char('Un-Taxed Sequence')
+    invoice_sequence = fields.Char('Serial')
 
     @api.multi
     def action_invoice_open(self):
         res = super(AccountInvoiceInhh, self).action_invoice_open()
         for invoice in self:
-            if invoice.state == 'open' and not invoice.tax_line_ids:
-                invoice.seq_un_taxed = self.env['ir.sequence'].next_by_code('untaxed.invoice.seq')
+            if invoice.state == 'open' and not invoice.tax_line_ids and not invoice.invoice_sequence:
+                invoice.invoice_sequence = self.env['ir.sequence'].next_by_code('untaxed.invoice.seq')
+            if invoice.state == 'open' and invoice.tax_line_ids:
+                invoice.invoice_sequence = self.env['ir.sequence'].next_by_code('taxed.invoice.seq')
         return res
+
+    @api.multi
+    def get_amount_total_words(self):
+        for val in self:
+            if val.amount_total:
+                words = num2words(val.amount_total, lang='ar')
+                return words
