292c712af782bf438befc29c5b927482ee03673d
ibrahim qabeel <ibrahimqabeell@gmail.com>
2019-07-08 14:53:21 +0200

    [UPD] Module: bi_customer_type, fixing related field issue

--- bi_customer_type/models/inherit_account_invoice.py
+++ bi_customer_type/models/inherit_account_invoice.py
@@ -11,7 +11,22 @@ class AccountInvoice(models.Model):
 class AccountInvoiceReport(models.Model):
     _inherit = 'account.invoice.report'
 
-    customer_type = fields.Many2one('customer.type', string="Customer Type", related='partner_id.customer_type', store=True)
+    def _get_customer_type(self):
+        if self.partner_id and self.partner_id.customer_type:
+            return self.partner_id.customer_type.id
+        else:
+            return False
+
+    @api.multi
+    @api.depends('partner_id.customer_type')
+    def _set_customer_type(self):
+        for record in self:
+            if record.partner_id and record.partner_id.customer_type:
+                record.customer_type = record.partner_id.customer_type.id
+            else:
+                record.customer_type = False
+
+    customer_type = fields.Many2one('customer.type', string="Customer Type", default=_get_customer_type)
 
     def _select(self):
         return super(AccountInvoiceReport, self)._select() + ", sub.customer_type as customer_type"
