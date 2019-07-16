5adf6d0aaf63b430d6cb30192530553c4efafc28
ibrahim qabeel <ibrahimqabeell@gmail.com>
2019-07-16 13:55:15 +0200

    [ADD] Module: bi_automatic_picking_cancel.

---
+++ bi_automatic_picking_cancel/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "BI Automatic Cancel Pickings",
+    'summary': """BI Automatic Cancel Pickings for (Credit Note)""",
+    'description': """
+        1- Automatic Cancel Pickings for (Credit Note).\n
+    """,
+    'author': "Bi Development Team",
+    'website': "http://bisolutions.com/",
+    'category': 'Account',
+    'version': '12.0.0.1.0',
+    'depends': ['account', 'stock', 'bi_accounting_customization'],
+    'data': [],
+    'sequence': 1,
+}
