957d4f583d1bee63d36208478aa53130fe89d523
ibrahim qabeel <ibrahimqabeell@gmail.com>
2019-07-21 13:32:52 +0200

    [UPD] Fixing re-compute pricelist discounts on credit note added to an invoice.

--- bi_bonus_vat/models/product.py
+++ bi_bonus_vat/models/product.py
@@ -2,16 +2,16 @@
 from odoo import models, fields, api, _
 
 
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    is_bonus = fields.Boolean(string="Is Bonus")
+    original_product = fields.Many2one('product.template', string="Original Product")
+
+
 class ProductProduct(models.Model):
     _inherit = "product.product"
 
     is_bonus = fields.Boolean(string="Is Bonus", related="product_tmpl_id.is_bonus")
     original_product = fields.Many2one('product.template', string="Original Product",
                                        related="product_tmpl_id.original_product")
-
-
-class ProductTemplate(models.Model):
-    _inherit = "product.template"
-
-    is_bonus = fields.Boolean(string="Is Bonus")
-    original_product = fields.Many2one('product.template', string="Original Product")
