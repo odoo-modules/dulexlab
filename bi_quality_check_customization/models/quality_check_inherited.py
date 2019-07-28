d87f499bc235ef0b515e20d40de712fc58d3d0b9
ibrahim qabeel <ibrahimqabeell@gmail.com>
2019-07-28 15:18:41 +0200

    [UPD] update module: bi_quality_check_customization.

--- bi_quality_check_customization/models/quality_check_inherited.py
+++ bi_quality_check_customization/models/quality_check_inherited.py
@@ -6,13 +6,11 @@ class InheritedQualityCheck(models.Model):
     _inherit = 'quality.check'
 
     move_id = fields.Many2one('stock.move', compute="get_picking_data")
-    move_line_id = fields.One2many('stock.move.line', related="move_id.move_line_ids")
-    # lot_ids = fields.Char(string="Lot/Serial No.", related='move_line_id.lot_name')
+    move_line_ids = fields.One2many('stock.move.line', 'quality_check_id', related="move_id.move_line_ids")
     partner_id = fields.Many2one('res.partner', string="Partner", related='picking_id.partner_id')
     initial_demand = fields.Float(string="Initial Demand", related='move_id.product_uom_qty')
     done_qty = fields.Float(string="Qty Done", related='move_id.quantity_done')
     uom_qty = fields.Many2one('uom.uom', string="Unit Of Measure", related='move_id.product_uom')
-
 
     @api.multi
     def get_picking_data(self):
@@ -21,9 +19,11 @@ class InheritedQualityCheck(models.Model):
             for move in point.picking_id.move_ids_without_package:
                 if point.product_id == move.product_id and move.product_id:
                     point.move_id = move.id
+                    point.move_line_ids = move.move_line_ids.ids
                     flag = True
             if not flag:
                 point.move_id = False
+                point.move_line_ids = False
 
 
 class InheritedStockMove(models.Model):
@@ -35,4 +35,4 @@ class InheritedStockMove(models.Model):
 class InheritedStockMoveLine(models.Model):
     _inherit = 'stock.move.line'
 
-    quality_check_id = fields.One2many('quality.check', 'move_line_id')
+    quality_check_id = fields.Many2one('quality.check')
