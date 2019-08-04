a6ec707061d51626b9d7a4b3f950d7761fd38b91
ibrahim qabeel <ibrahimqabeell@gmail.com>
2019-08-04 11:52:35 +0200

    [ADD] Module: bi_pl_report_customization.

---
+++ bi_pl_report_customization/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "BI P&L Report Customizations",
+    'summary': "BI P&L Report Customizations",
+    "version": "1",
+    'sequence': 1,
+    "author": "BI Solutions Development Team",
+    'license': 'LGPL-3',
+    "category": "Accounting Report",
+    "depends": ["account", "account_reports"],
+    "data": [
+        'static/src/xml/report_financial_inherit.xml',
+    ],
+    "installable": True
+}
+
+
---
+++ bi_pl_report_customization/models/account_financial_report.py
@@ -0,0 +1,77 @@
+from odoo import models, fields, api, _
+from odoo.tools.misc import formatLang
+
+
+class AccountFinancialReportLine(models.Model):
+    _inherit = "account.financial.html.report.line"
+
+    calculation_mab = {
+        'net_sales': 0.0,
+        'total_expenses': 0.0,
+        'total_interest': 0.0,
+        'total_general_tax': 0.0,
+        'total_other_income': 0.0,
+    }
+    @api.multi
+    def _get_lines(self, financial_report, currency_table, options, linesDicts):
+        res = super(AccountFinancialReportLine, self)._get_lines(financial_report, currency_table, options, linesDicts)
+        if financial_report.name in ['Income Statement', 'الأرباح و الخسائر']:
+            for line in res:
+                for i, col in enumerate(line['columns']):
+                    if i > 0:
+                        break
+                    field_name = ''
+                    if 'no_format_name' in col:
+                        field_name = 'no_format_name'
+                    elif 'name' in col and col['name'] and (type(col['name']) == float):
+                        field_name = 'name'
+                    if field_name:
+                        if line['name'] in ['الدخل التشغيلي', 'Net Sales']:
+                            self.calculation_mab['net_sales'] = float(col[field_name])
+                        elif line['name'] in ['الإجمالي Expenses', 'Total Expenses']:
+                            self.calculation_mab['total_expenses'] = float(col[field_name])
+                        elif line['name'] in ['الإجمالي Interest', 'Total Interest']:
+                            self.calculation_mab['total_interest'] = float(col[field_name])
+                        elif line['name'] in ['الإجمالي General Tax', 'Total General Tax']:
+                            self.calculation_mab['total_general_tax'] = float(col[field_name])
+                        elif line['name'] in ['الإجمالي Other Income', 'Total Other Income']:
+                            self.calculation_mab['total_other_income'] = float(col[field_name])
+
+            for line in res:
+                for i, col in enumerate(line['columns']):
+                    line_name = ''
+                    if line['name'] in [
+                        'الدخل التشغيلي', 'Net Sales', 'تكاليف الدخل', 'Cost of Goods Sold (COGS)',
+                        'الإجمالي إجمالي الربح', 'Total Gross Profit', 'الإجمالي الدخل', 'Total Income',
+                        'الإجمالي المصروفات', 'Total Expenses and Depreciation', 'صافي الربح',
+                        'Net Profit Before Interest', 'الإجمالي Interest', 'Total Interest',
+                        'Net Profit Before Tax', 'الإجمالي General Tax', 'Total General Tax',
+                        'الإجمالي Other Income', 'Total Other Income', 'Net Profit after Tax'
+                    ]:
+                        line_name = 'net_sales'
+                    elif line['name'] in [
+                        'Salaries Expense', 'Admin Salaries Expenses', 'Admin Expenses',
+                        'Distribution Expenses', 'Hr Expenses', 'Regestration Expenses', 'Marketing Expenses',
+                        'Other Marketing Expenses', 'Factory expenses', 'الإجمالي Expenses', 'Total Expenses',
+                        'إستهلاك', 'Depreciation'
+                    ]:
+                        line_name = 'total_expenses'
+                    elif line['name'] in ['Bank Interest', 'Capital Interest']:
+                        line_name = 'total_interest'
+                    elif line['name'] in ['TAX']:
+                        line_name = 'total_general_tax'
+                    elif line['name'] in ['Other Income']:
+                        line_name = 'total_other_income'
+
+                    if 'no_format_name' in col:
+                        if self.calculation_mab[line_name] == 0.0:
+                            col['percent'] = 0.00
+                        else:
+                            col['percent'] = '{0:,.2f}'.format(100 * (float(col['no_format_name']) / self.calculation_mab[line_name]))
+                    elif self._context.get('print_mode') and self._context.get('no_format') and not self._context.get('prefetch_fields') and (col['name'] or col['name'] == 0.0):
+                        if type(col['name']) is float:
+                            if self.calculation_mab[line_name] == 0.0:
+                                col['col_name'] = '{0:,.2f}'.format(col['name']) + '  - 0.00%'
+                            else:
+                                col['col_name'] = '{0:,.2f}'.format(col['name']) + '  - ' + '{0:,.2f}'.format(100 * (float(col['name']) / self.calculation_mab[line_name])) + '%'
+        return res
