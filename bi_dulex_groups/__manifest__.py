5e00fbd665b212052ccafee8f517cc553f46467b
ibrahimz <ibrahimqabeell@gmail.com>
2019-08-18 13:30:35 +0200

    [ADD] Module: bi_dulex_groups.

---
+++ bi_dulex_groups/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "BI Dulex Groups",
+    'summary': "BI Dulex Groups",
+    'description': """  
+            This module adds 3 new groups to sales:
+            - margin access
+            - cost access
+            - sales price access
+     """,
+    'author': "BI Solutions Development Team",
+    'category': 'Sales',
+    'version': '0.1',
+    'depends': ['sale', 'stock', 'sale_margin', 'base'],
+    'data': [
+        'security/groups.xml',
+        'views/inherit_saleorder_view.xml',
+        'views/inherit_productproduct_view.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'sequence': 1
+}
